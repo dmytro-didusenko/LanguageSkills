54a141a7daaa97ac2bb69eb2aafa957407e3254e
VyacheslavDiedov <diedov270@gmail.com>
2021-01-27 00:39:14 +0200

    Added rootPath

--- BusinessLogicLayer/Initializer/Interfaces/IInitialize.cs
+++ BusinessLogicLayer/Initializer/Interfaces/IInitialize.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using DataAccessLayer.DataBaseModels;
 
 namespace BusinessLogicLayer.Initializer.Interfaces
 {
     interface IIInitialize<T>
     {
         //void AddMaterial<T>(T item);
+        List<T> GetDataFromFile();
     }
 }
